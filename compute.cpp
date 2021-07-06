60559a91a5754b4884d33e6b22120bc7d802e3b5
LaraPompei <pompei.lara@engenharia.ufjf.br>
2021-07-05 23:33:36 -0300

    Correção compute e main
    Adicionei comentários para debugar, os mesmos vão ser removidos futuramente

--- compute.cpp
+++ compute.cpp
@@ -30,11 +30,11 @@ void compute(const FullEnvironment& env){
      //SIP:
   
     //instantiating the parameter space
-    
+    cerr<<"instantiating the parameter space.."<<endl;    
     VectorSpace<> paramSpace(env, "param_", 1, NULL);
     
     //instantiating the parameter domain
-    
+    cerr<<"instantiating the parameter domain"<<endl;
     GslVector paramMinValues(paramSpace.zeroVector());
     GslVector paramMaxValues(paramSpace.zeroVector());
     paramMinValues[0] = 0.0;
@@ -42,7 +42,7 @@ void compute(const FullEnvironment& env){
     BoxSubset<> paramDomain("param_", paramSpace, paramMinValues, paramMaxValues);
     
     //instantiating the likelihood function object
-
+    cerr<<"instantiating the likelihood function object and generating the samples"<<endl;
     double spacing_points = 0.5;
     int number_samples    = 10000;
     double* t             = new double[tam];
@@ -55,18 +55,23 @@ void compute(const FullEnvironment& env){
     int poi = 3; //point of interest
 
     //memory allocation for the matrix data [number_samplesxtam]
+    cerr<<"allocating memory for the data matrix"<<endl;
     for(int i = 0; i<number_samples; i++){
         data[i] = new double[tam];
     }
 
     //vector t is filled with [0.0,5.5] interval catching each number after 0.5
+    cerr<<"Filling vector t"<<endl;
     for(int i =0; i<tam;i++){
         t[i] = (i)*spacing_points;
     }
 
     //Generating and fillling the matrix, which the lanes represent the samples(yvalues) and the columns represent the date in each point of time(t)
-    filling_matrix(number_samples, data_mean, t, data);
+    cerr<<"Generating and filling the matrix"<<endl;
+    filling_matrix(number_samples, t, values_a, data);
+
     //mean of data
+    cerr<<"Calculating the mean of the data"<<endl;
     for(int j=0; j < tam; j++){
         for(int i = 0; i < tam; i++){
             data_mean[j] += data[i][j];
@@ -77,6 +82,7 @@ void compute(const FullEnvironment& env){
     }
 
     //data standard deviation
+    cerr<<"Calculating the standard deviation"<<endl;
     for(int j = 0; j < tam; j++){
         for(int i = 0; i<number_samples; i++){
             data_std[j] += pow((data[i][j] - data_mean[j]),2);
@@ -86,23 +92,25 @@ void compute(const FullEnvironment& env){
         data_std[i] = sqrt(data_std[i]/number_samples);
         cout<<"Para t = "<<i<<endl<<"Data mean: "<<data_mean[i]<<endl<<"Data standard deviation: "<<data_std[i]<<endl;
     }
-
+    cerr<<"Creating the likelihood object"<<endl;
     Likelihood<> lhood("like_", paramDomain, data_mean, t, data_std, poi);
 
     //define the prior RV
+    cerr<<"Defining the prior RV"<<endl;
     UniformVectorRV<> priorRv("prior_", paramDomain);
 
     //instantiate the inverse problem
+    cerr<<"Instantiating the inverse problem"<<endl;
     GenericVectorRV<> postRv("post_", paramSpace);
     StatisticalInverseProblem<> ip("", NULL, priorRv, lhood, postRv);
 
     //Solve the inverse problem
-    //set the 'pdf' and the 'realizer' of the posterior RV
+    cerr<<"Solving the inverse problem"<<endl;
     GslVector paramInitials(paramSpace.zeroVector());
     priorRv.realizer().realization(paramInitials);
 
     GslMatrix proposalCovMatrix(paramSpace.zeroVector());
-    proposalCovMatrix(0,0) = std::pow(std::abs(paramInitials[0])/20.0, 2.0);
+    proposalCovMatrix(0,0) = pow(abs(paramInitials[0])/20.0, 2.0);
 
     ip.solveWithBayesMetropolisHastings(NULL, paramInitials, &proposalCovMatrix);
     
